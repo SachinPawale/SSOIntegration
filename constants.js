ec9c4a90f8e2df2a4bd6aa6eec027e8058c644f6
SachinPawale <sachin.pawale225@gmail.com>
2022-07-22 15:44:27 +0530

    SSO changes

--- constants.js
+++ constants.js
@@ -1,8 +1,12 @@
-module.exports ={
-    PORT_NO : 3001,
-    DB_USER:'root',
-    DB_PASS:'summerof2010',
-    DB_HOST:'localhost',
-    DATABASE:'ltcportal',
-    DB_CONNECTION_LIMIT :10
+module.exports = {
+    PORT_NO: 3000,
+    DB_USER: 'root',
+    DB_PASS: 'summerof2010',
+    DB_HOST: 'localhost',
+    DATABASE: 'ltcportal',
+    DB_CONNECTION_LIMIT: 10,
+    SSO_ENTRY_POINT_URL: 'https://lightstormtelecom.awsapps.com/start',
+    SSO_LOGOUT_URL: 'https://portal.sso.ap-south-1.amazonaws.com/saml/logout/MTAyODgxNzE3MjkyX2lucy02ZjZiMzYwMmJjYWM3NTFl',
+    SSO_ISSUER_URL: 'https://portal.sso.ap-south-1.amazonaws.com/saml/assertion/MTAyODgxNzE3MjkyX2lucy02ZjZiMzYwMmJjYWM3NTFl',
+    SSO_CALLBACK_URL:'https://csrg.lightstorm.in/ltc/ssoapi/login/callback'
 }
--- index.js
+++ index.js
@@ -4,54 +4,88 @@ const express = require('express');
 const app = express()
 const pool = require('./db')
 const constants = require('./constants')
+const expressSession = require('express-session')
+const xml2js = require('xml2js');
+const flash = require('connect-flash')
+
 
 app.use(express.json())
+
+app.use(express.urlencoded({ extended: false }))
+app.use(expressSession({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'cat'
+}));
+
 passport.serializeUser(function (user, done) {
+    console.log('userrrrrrrrrrrr', user)
     done(null, user);
 });
 
 passport.deserializeUser(function (user, done) {
+    console.log('userrrrrrrrrrrr1111111111111', user)
     done(null, user);
 });
+
+
 // app.use(express.urlencoded())
 
-let cert1 = require('fs').readFileSync(__dirname + '/testFiles/AWS_SSO_for_Custom SAML 2.0 application_certificate.pem', 'utf8')
+//let cert1 = require('fs').readFileSync(__dirname + '/testFiles/AWS_SSO_for_Custom SAML 2.0 application_certificate.pem', 'utf8')
 
 let cert = require('fs').readFileSync(__dirname + '/AWS_SSO_for_Custom SAML 2.0 application_certificate.pem', 'utf8')
 var samlStrategy = new SamlStrategy({
-    //callbackUrl: 'http://43.204.223.103:3001/api/login/callback',
-    callbackUrl: 'https://csrg.lightstorm.in/ltc/ssoapi1/login/callback',
-    //entryPoint:'https://portal.sso.ap-south-1.amazonaws.com/saml/assertion/MTAyODgxNzE3MjkyX2lucy02ZjZiMzYwMmJjYWM3NTFl',
-    entryPoint: 'https://portal.sso.ap-south-1.amazonaws.com/saml/assertion/MDYxNTg0ODc3NTQ4X2lucy1jNjUzODVlM2EwZTAxZjhm',
-    issuer: 'https://portal.sso.ap-south-1.amazonaws.com/saml/assertion/MDYxNTg0ODc3NTQ4X2lucy1jNjUzODVlM2EwZTAxZjhm',
-    logoutUrl:'https://portal.sso.ap-south-1.amazonaws.com/saml/logout/MDYxNTg0ODc3NTQ4X2lucy1jNjUzODVlM2EwZTAxZjhm',
-    cert: cert1
+    callbackUrl: constants.SSO_CALLBACK_URL,
+    entryPoint: constants.SSO_ENTRY_POINT_URL,
+    issuer: constants.SSO_ISSUER_URL,
+    logoutUrl: constants.SSO_LOGOUT_URL,
+    cert: cert
 }, function (profile, done) {
     console.log('Profile: %j', profile);
     return done(null, profile);
 });
 
 passport.use(samlStrategy);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash())
 
-app.get('ssoapi1/login/fail', (req, res) => res.send(`<p> test </p>`))
+app.get('/ssoapi/login/fail', (req, res) => res.send(`${req.flash("error")}  <p> test </p>`))
 
-app.get('ssoapi1/', (req, res) => res.send(`<p> AttemptedUrl for Newel </p>`))
+app.get('/ssoapi', (req, res) => res.send(`${req.flash("info")} <p> AttemptedUrl </p>`))
 
 //app.get('/.well-known/pki-validation/:Id', (req, res) => )
 
 
-app.get('/ssoapi1/',
-    passport.authenticate('saml', { failureRedirect: 'ssoapi1/login/fail' }),
-    function (req, res) {
+app.get('/ssoapi/',
+    passport.authenticate('saml', { failureRedirect: '/ssoapi/login/fail' }),
+    (req, res) => {
         res.send('Hello World!');
     }
 );
 
-app.post('/ssoapi1/login/callback',
-    passport.authenticate('saml', { failureRedirect: 'ssoapi1/login/fail', failureFlash: true }),
-    function (req, res) {
-        console.log('reqqqqqqqqq', req)
-        res.redirect('/');
+app.post('/ssoapi/login/callback',
+    passport.authenticate('saml', { failureRedirect: '/ssoapi/login/fail', failureFlash: true }),
+    async (req, res) => {
+        try {
+            console.log('reqqqqqqqqq', req.headers)
+            let xmlData = Buffer.from(req.body.SAMLResponse, 'base64').toString()
+            //console.log('xmlData', xmlData)
+            let email = await parseXml(xmlData)
+            console.log("Emailllllllllllll", email)
+            req.flash('info', `Welcome ${email}`)
+            res.redirect('/ssoapi')
+            //add the email validation logic here in index.js
+
+            //res.send(JSON.stringify(req.body));
+        } catch (error) {
+            console.log('error', error)
+            req.flash('error', 'Access Denied')
+            res.redirect('/ssoapi/login/fail');
+
+        }
+
+
     }
 );
 
@@ -76,3 +110,20 @@ app.post('/ssoapi1/login/callback',
 
 
 app.listen(constants.PORT_NO, () => console.log("Server is listening on port", constants.PORT_NO))
+
+async function parseXml(xmlData) {
+    return new Promise((resolve, reject) => {
+        let parser = new xml2js.Parser();
+        parser.parseString(xmlData, function (err, result) {
+            //Extract the value from the data element
+            //extractedData = result['config']['data'];
+            if (err) {
+                reject(err)
+            } else {
+                resolve(result["saml2p:Response"]["saml2:Assertion"][0]["saml2:Subject"][0]["saml2:NameID"][0]["_"]);
+
+            }
+            //console.log(result);
+        })
+    })
+}
